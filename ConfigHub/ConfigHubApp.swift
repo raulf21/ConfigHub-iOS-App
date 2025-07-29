d2b0a04518a7e1c8cf49b8046e34b3a5c9015b39
raulf21 <raulflores650@gmail.com>
2025-07-29 11:30:20 -0700

    Initial Commit

---
+++ ConfigHub/ConfigHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfigHubApp.swift
+//  ConfigHub
+//
+//  Created by Raul Flores on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ConfigHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConfigHubTests/ConfigHubTests.swift
@@ -0,0 +1,16 @@
+//
+//  ConfigHubTests.swift
+//  ConfigHubTests
+//
+//  Created by Raul Flores on 7/29/25.
+//
+
+import Testing
+
+struct ConfigHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
